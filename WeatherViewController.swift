c425f5c64e049b7a3446a34377bf5d527f275cde
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-08-17 16:29:12 -0700

    Add files via upload
    getting project files uploaded

---
+++ WeatherViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+    @IBAction func searchPressed(_ sender: UIButton) {
+        //will dismiss the keyboard after tyoing
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    //This is a predefined fxn that allows us to handle the input
+    //from the return button on the keyboard.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+
+    //Will dismiss the keyboard when return is pressed
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //print(searchTextField.text!)
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Please enter a city name"
+            return false
+        }
+    }
+}
+

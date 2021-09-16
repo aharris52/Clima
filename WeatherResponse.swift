d942ccf365bf3867199fae3ea9ec83a67766d9ac
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-09-16 12:51:42 -0700

    Created weather model object. processes condition codes. prints formatted temp string.

---
+++ WeatherResponse.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Andrew Harris on 8/27/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherResponse: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+    let id: Int
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description: String
+    
+}

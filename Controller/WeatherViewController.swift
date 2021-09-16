c2cfe60a202ced0bfff4a6f965a8ca9c16d446df
Andrew Harris <59749069+aharris52@users.noreply.github.com>
2021-09-16 15:19:34 -0700

    Refactored to incorporate protocol/delegate pattern

--- Controller/WeatherViewController.swift
+++ Controller/WeatherViewController.swift
@@ -2,23 +2,29 @@
 //  ViewController.swift
 //  Clima
 //
+//  Application built as an educational project
+//  to learn iOS development.
+//
 //  Created by Angela Yu on 01/09/2019.
 //  Copyright © 2019 App Brewery. All rights reserved.
 //
 
 import UIKit
 
-class WeatherViewController: UIViewController, UITextFieldDelegate {
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchTextField.delegate = self
+        weatherManager.delegate = self
     }
     @IBAction func searchPressed(_ sender: UIButton) {
         //will dismiss the keyboard after tyoing
@@ -37,6 +43,10 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     //Will dismiss the keyboard when return is pressed
     func textFieldDidEndEditing(_ textField: UITextField) {
         //print(searchTextField.text!)
+        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
         searchTextField.text = ""
     }
     
@@ -47,6 +57,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
             textField.placeholder = "Please enter a city name"
             return false
         }
+    }
+    func didUpdateWeather(weather: WeatherModel) {
+        print(weather.temperature)
     }
 }
 
